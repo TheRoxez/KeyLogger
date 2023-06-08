85a2359afdf0bcc0d5707fafe5ddeb3510d7adfd
Muhammet Ali Bozkurt <128151296+TheRoxez@users.noreply.github.com>
2023-06-08 12:10:03 +0300

    Add files via upload

---
+++ klggr.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace klggr
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                    char key = keyInfo.KeyChar;
+
+                    LogWrite(key);
+
+                }
+            }
+        }
+        static void LogWrite(char message)
+        {
+            string dyol = AppDomain.CurrentDomain.BaseDirectory + "/Logs";
+            if (!Directory.Exists(dyol))
+            {
+                Directory.CreateDirectory(dyol);
+            }
+            string txtyol = AppDomain.CurrentDomain.BaseDirectory + "/Logs/KLG.txt";
+            if (!File.Exists(txtyol))
+            {
+                using (StreamWriter sv = File.CreateText(txtyol))
+                {
+                    sv.Write(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sv = File.AppendText(txtyol))
+                {
+                    sv.Write(message);
+                }
+            }
+        }
+    }
+}
